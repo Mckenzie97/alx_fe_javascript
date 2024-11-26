e20abe21ce52cfb29d4ffcf3e18f1cb8cdd707e0
Emmanuel Makinde <emmanueltofunmi9@gmail.com>
2024-11-26 14:05:20 +0100

    fast

---
+++ dom-manipulation/script.js
@@ -0,0 +1,29 @@
+const quotes = [
+    { text: "The best way to predict the future is to create it.", category: "Inspiration" },
+    { text: "Life is 10% what happens to us and 90% how we react to it.", category: "Motivation" },
+    { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", category: "Success" },
+  ];
+  
+  
+  function showRandomQuote() {
+    const quoteDisplay = document.getElementById('quoteDisplay');
+    if (quotes.length === 0) {
+      quoteDisplay.textContent = "No quotes available.";
+      return;
+    }
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    quoteDisplay.textContent = `"${quote.text}" - ${quote.category}`;
+  }
+  
+  
+  function addQuote() {
+    const newQuoteText = document.getElementById('newQuoteText').value.trim();
+    const newQuoteCategory = document.getElementById('newQuoteCategory').value.trim();
+  
+    if (newQuoteText === "" || newQuoteCategory === "") {
+      alert("Both fields are required to add a quote.");
+      return;
+    }
+  
+}
